7bdf80ce46901a636afb0efcd5bf521fe6dbe545
Angel J. Haro <haro.j.angel@gmail.com>
2024-04-26 18:09:05 -0700

    database: neo4j

---
+++ accessingdataneo4j/src/test/java/io/aharoj/accessingdataneo4j/Accessingdataneo4jApplicationTests.java
@@ -0,0 +1,13 @@
+package io.aharoj.accessingdataneo4j;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Accessingdataneo4jApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
