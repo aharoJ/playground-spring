858c3a69816b28731b0e12a9e933384981cd9c48
Angel J. Haro <haro.j.angel@gmail.com>
2024-04-27 12:34:51 -0700

    relational db

---
+++ relationaldataaccess/src/test/java/io/aharoj/relationaldataaccess/RelationaldataaccessApplicationTests.java
@@ -0,0 +1,13 @@
+package io.aharoj.relationaldataaccess;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RelationaldataaccessApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
