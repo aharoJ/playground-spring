d3c9b9d744cb814d280843e221b4f995ea693d3f
Angel J. Haro <haro.j.angel@gmail.com>
2024-04-27 02:50:13 -0700

    interface

---
+++ jobapplication/src/main/java/io/aharoj/jobapplication/job/JobService.java
@@ -0,0 +1,14 @@
+package io.aharoj.jobapplication.job;
+
+import java.util.List;
+
+/**
+ * JobService
+ */
+public interface JobService {
+
+  List<Job> findAll();
+
+  void createJob(Job job);
+
+}
