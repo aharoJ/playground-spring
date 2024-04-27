99f35aab70251e4805085d9f02bda6c0c9f31273
Angel J. Haro <haro.j.angel@gmail.com>
2024-04-27 12:34:15 -0700

    validating formatinput

---
+++ validatingforminput/src/test/java/io/aharoj/validatingforminput/ValidatingforminputApplicationTests.java
@@ -0,0 +1,13 @@
+package io.aharoj.validatingforminput;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ValidatingforminputApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
