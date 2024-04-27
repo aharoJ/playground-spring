c36bd3ea60fffa87e65d7b91c816d0fca39b1cc5
Angel J. Haro <haro.j.angel@gmail.com>
2024-04-27 03:13:33 -0700

    fixing unique ID

--- jobapplication/src/main/java/io/aharoj/jobapplication/job/impl/JobServiceImpl.java
+++ jobapplication/src/main/java/io/aharoj/jobapplication/job/impl/JobServiceImpl.java
@@ -3,15 +3,21 @@ package io.aharoj.jobapplication.job.impl;
 import java.util.ArrayList;
 import java.util.List;
 
+import org.springframework.stereotype.Service;
+
 import io.aharoj.jobapplication.job.Job;
 import io.aharoj.jobapplication.job.JobService;
+import lombok.AllArgsConstructor;
+import lombok.NoArgsConstructor;
 
 /**
  * JobServiceImpl
  * this promomotes loose coupling between the controller and the service
  */
+@Service
 public class JobServiceImpl implements JobService {
   private List<Job> jobs = new ArrayList<>();
+  private Long nextId = 1L;
 
   @Override
   public List<Job> findAll() {
@@ -20,6 +26,7 @@ public class JobServiceImpl implements JobService {
 
   @Override
   public void createJob(Job job) {
+    job.setId(nextId++);
     jobs.add(job);
   }
 
