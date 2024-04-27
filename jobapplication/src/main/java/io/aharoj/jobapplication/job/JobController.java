8e0887e56c29618f32dc75b21b37cfbcdd6ec614
Angel J. Haro <haro.j.angel@gmail.com>
2024-04-27 03:28:10 -0700

    return jobs by ID

--- jobapplication/src/main/java/io/aharoj/jobapplication/job/JobController.java
+++ jobapplication/src/main/java/io/aharoj/jobapplication/job/JobController.java
@@ -4,9 +4,12 @@ import java.util.ArrayList;
 import java.util.List;
 
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
+
+import lombok.AllArgsConstructor;
 
 /**
  * JobController
@@ -14,16 +17,28 @@ import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class JobController {
+  public JobController(JobService jobService) {
+    this.jobService = jobService;
+  }
+
+  private JobService jobService;
 
   @GetMapping("/jobs")
   public List<Job> findAll() {
-    return jobs;
+    return jobService.findAll();
   }
 
   @PostMapping("/jobs")
-  public String createJob(@RequestBody Job job){
-    jobs.add(job);
+  public String createJob(@RequestBody Job job) {
+    jobService.createJob(job);
     return "Job added succesfully";
+  }
+
+  @GetMapping("/jobs/{id}")
+  // whatever path variable is passed in the url, it will be passed to the method
+  public Job getJobById(@PathVariable Long id) {
+    Job job = jobService.getJobById(id);
+    return job;
   }
 
 }
