9743dc778f22c789a85d130dd4292d7e00141701
Angel J. Haro <haro.j.angel@gmail.com>
2024-04-27 12:34:58 -0700

    jms

---
+++ messagingjms/src/test/java/io/aharoj/messagingjms/MessagingjmsApplicationTests.java
@@ -0,0 +1,13 @@
+package io.aharoj.messagingjms;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MessagingjmsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
